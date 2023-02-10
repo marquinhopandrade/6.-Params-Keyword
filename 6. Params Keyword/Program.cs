ebc4057912027cf98628abe205e466e4b50dfd4f
Marcos Andrade <mvpdeandrade@gmail.com>
2023-02-10 16:11:32 -0300

    Adiciona função que recebe qualquer tipo de objeto e imprime com espaços

--- 6. Params Keyword/Program.cs
+++ 6. Params Keyword/Program.cs
@@ -6,6 +6,12 @@ namespace _6._Params_Keyword
     {
         static void Main(string[] args)
         {
+            int price = 50;
+            float pi = 3.14f;
+            char at = '@';
+            string book = "The Hobbit";
+            ParamsMethod2(price, pi, at, book);
+            ParamsMethod2("Hello", 5.3, '$');
             ParamsMethod("this", " is", " a ", " long", " string.", " I", " have", " no", " idea", " when", " it's", " going", " to", " end", "...");
         }
         public static void ParamsMethod(params string[] sentence)
@@ -14,6 +20,16 @@ namespace _6._Params_Keyword
             {
                 Console.Write(sentence[i] + " ");
             }
+        }
+        public static void ParamsMethod2(params object[] stuff)
+        {
+            //foreach loop to go through the array of objects
+            foreach (object obj in stuff)
+            {
+                //print each object followed by a space
+                Console.WriteLine(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
